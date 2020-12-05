db1df8d8fce0effadb45609ba8ff9272aeb790b7
Maksym Balaba <jsmaks26@gmail.com>
2020-12-05 17:56:34 +0200

    dz-2

---
+++ js/task-4.js
@@ -0,0 +1,25 @@
+const formatString = function (string) {
+    const maxWords = 40;
+
+    if (string.length > maxWords) {
+        string = string.slice(0, 39);
+        string = `${string}...`
+    }
+    return string;
+}
+
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// вернется оригинальная строка
+
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// вернется форматированная строка
+
+console.log(formatString('Curabitur ligula sapien.'));
+// вернется оригинальная строка
+
+console.log(
+    formatString(
+        'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
+    ),
+);
+//вернется форматированная строка
